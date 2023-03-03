71ed6e9ca9d91cfa4752d6479ee43404d8ac55a3
hetj003 <het@vebuin.com>
2023-03-03 15:17:48 +0530

    This commit is for calling main on create and destroy action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,13 +4,13 @@ class RelationshipsController < ApplicationController
   before_action :set_user
 
   def create
-    # RelationshipMailer.started_following_user(@user).deliver_now
+    RelationshipMailer.started_following_user(@user).deliver_now
     current_user.follow(@user)
     redirect_to profile_path(@user)
   end
   
   def destroy
-    # RelationshipMailer.stops_following_user(@user).deliver_now
+    RelationshipMailer.stops_following_user(@user).deliver_now
     current_user.unfollow(@user)
     redirect_to profile_path(@user)
   end
